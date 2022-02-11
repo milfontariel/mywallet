dc5470af5fa5a80bd6e9caae6fb1aaadd26476e8
mrcs22 <marcusmoraes2010@hotmail.com>
2022-02-11 11:41:28 -0300

    Preparando revisão

--- src/Contexts.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const Contexts = createContext();
-
-export default Contexts;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'normalize.css';
-
-ReactDOM.render(<App />, document.querySelector(".root"));
